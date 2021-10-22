b83d154c5f949cdccd6d16f832cf187dc6ed5ce0
mgorny01 <mateusz.gorny@isobar.com>
2021-10-22 18:02:26 +0200

    feat: eslint, prettier and husky configuration

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Login from './components/Login';
 
 function App() {
-  const [isLogged, setIsLogged] = useState(false);
+	const [isLogged, setIsLogged] = useState(false);
 
-  useEffect(() => {
-    if (localStorage.getItem('userName')) {
-      setIsLogged(true);
-    }
-  }, []);
+	useEffect(() => {
+		if (localStorage.getItem('userName')) {
+			setIsLogged(true);
+		}
+	}, []);
 
-  const loginHandler = (user) => {
-    console.log(user);
-    localStorage.setItem('userName', user);
-    setIsLogged(true);
-  };
+	const loginHandler = (user) => {
+		console.log(user);
+		localStorage.setItem('userName', user);
+		setIsLogged(true);
+	};
 
-  return (
-    <>
-      {!isLogged && <Login userLoginHandler={loginHandler} />}
-      {isLogged && <p>aplikacja po zalogowaniu</p>}
-    </>
-  );
+	return (
+		<>
+			{!isLogged && <Login userLoginHandler={loginHandler} />}
+			{isLogged && <p>aplikacja po zalogowaniu</p>}
+		</>
+	);
 }
 
 export default App;
-
