190c81d0de8364b6bccae91ae1b9059ba751ef49
mgorny01 <mateusz.gorny@isobar.com>
2021-10-28 13:27:49 +0200

    feat: add handling to display entries filtered by desired date

--- src/Screens/Calendar/Calendar.jsx
+++ src/Screens/Calendar/Calendar.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from '@mui/material';
 
 import { useGetEntries } from '../../queries';
 
+const getTimeStamp = () => {
+  const timeStamp = new Date();
+
+  timeStamp.setHours(2, 0, 0, 0, 0);
+
+  return timeStamp;
+};
+
 const Calendar = () => {
+  const [today, setToday] = useState(getTimeStamp());
   const { data } = useGetEntries({
-    date: '2021-10-28T00:00:00.000Z',
+    date: today,
   });
 
-  console.log(data);
+  const handleDateButtonClick = (direction) => {
+    setToday((prevDate) => {
+      const newDate = new Date(prevDate);
+      newDate.setDate(newDate.getDate() + direction);
+
+      return newDate;
+    });
+  };
 
   return (
     <div>
+      <button onClick={() => handleDateButtonClick(-1)}>Prev</button>
+      <h1>
+        {today?.toLocaleDateString('pl-pl', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        })}
+      </h1>
+      <button onClick={() => handleDateButtonClick(1)}>Next</button>
       <Container direction="column">
         {data?.entryMany?.map(({ _id, tag, order, startTime, endTime }) => (
           <li key={_id}>
