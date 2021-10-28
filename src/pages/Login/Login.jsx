1ab162b14e370cc8b439939190ade670f3ae779f
tomaszbilka <bila4@op.pl>
2021-10-28 19:09:08 +0200

    log in page layout

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,19 @@
 import React, { useState } from 'react';
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+
 import { setToLocalStorage } from '../../utils/localStorage';
+
+const imgStyle = {
+  width: '100px',
+  float: 'right',
+};
 
 const Login = ({ token, setAuthUser }) => {
   const [inputValue, setInputValue] = useState('');
@@ -15,10 +28,53 @@ const Login = ({ token, setAuthUser }) => {
   const handleInputOnChange = (e) => setInputValue(e.target.value);
 
   return (
-    <form onSubmit={handleLogin}>
-      <input onChange={handleInputOnChange} value={inputValue} type="text" />
-      <button>Log In</button>
-    </form>
+    <Container maxWidth="xs" style={{ marginTop: '20vh' }}>
+      <Typography
+        sx={{ fontSize: 28 }}
+        color="text.secondary"
+        style={{ textAlign: 'center' }}
+        gutterBottom
+      >
+        WORKLOG
+      </Typography>
+      <Card sx={{ minHeight: 375 }}>
+        <form onSubmit={handleLogin}>
+          <CardContent>
+            <img
+              style={imgStyle}
+              src="https://media.designrush.com/agencies/80874/conversions/Selleo-logo-profile.jpg"
+              alt="selleo login"
+            />
+            <Typography
+              sx={{ fontSize: 28 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              Log In
+            </Typography>
+            <TextField
+              id="outlined-basic"
+              label="user name"
+              variant="outlined"
+              onChange={handleInputOnChange}
+              value={inputValue}
+              style={{ marginTop: '10vh', width: '100%' }}
+            />
+          </CardContent>
+          <CardActions>
+            <Button
+              size="big"
+              variant="contained"
+              type="submit"
+              fullWidth="true"
+              style={{ marginTop: '16vh' }}
+            >
+              LogIn
+            </Button>
+          </CardActions>
+        </form>
+      </Card>
+    </Container>
   );
 };
 
