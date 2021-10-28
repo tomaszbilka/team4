3bf772b0388a414cbadba3835dcd04cc9643d938
mgorny01 <mateusz.gorny@isobar.com>
2021-10-28 13:04:16 +0200

    feat: add useGetEntries query and display results on calendar page

--- src/Screens/Calendar/Calendar.jsx
+++ src/Screens/Calendar/Calendar.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Container } from '@mui/material';
+
+import { useGetEntries } from '../../queries';
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  const { data } = useGetEntries({
+    date: '2021-10-28T00:00:00.000Z',
+  });
+
+  console.log(data);
+
+  return (
+    <div>
+      <Container direction="column">
+        {data?.entryMany?.map(({ _id, tag, order, startTime, endTime }) => (
+          <li key={_id}>
+            <p>Tag Name: {tag?.name}</p>
+            <p>Tag Bundle Name: {tag?.tagBundle?.name}</p>
+            <p>Order{order}</p>
+            <p>{startTime}</p>
+            <p>{endTime}</p>
+          </li>
+        ))}
+      </Container>
+    </div>
+  );
 };
 
 export default Calendar;
